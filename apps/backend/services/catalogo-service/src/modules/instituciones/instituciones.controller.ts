7571abe42f9df3d9ca9dd28cfa8f869d8c31f118
LennyRocha <20233tn094@utez.edu.mx>
2026-07-25 17:22:42 -0600

    [ADD] Backend de instituciones completado.

--- apps/backend/services/catalogo-service/src/modules/instituciones/instituciones.controller.ts
+++ apps/backend/services/catalogo-service/src/modules/instituciones/instituciones.controller.ts
@@ -1,19 +1,33 @@
 import {
+  Body,
   Controller,
   DefaultValuePipe,
+  Delete,
   Get,
+  HttpCode,
   Param,
   ParseBoolPipe,
   ParseIntPipe,
+  Patch,
+  Post,
   Query,
 } from '@nestjs/common';
 import { InstitucionesService } from './instituciones.service';
 import { ApiOperation, ApiParam, ApiTags, ApiQuery } from '@nestjs/swagger';
+import { UpdateInstitucionDto } from './dtos/update-insticuion.dto';
+import { CreateInstitucionDto } from './dtos/create-institucion.dto';
 
 @ApiTags('Instituciones')
 @Controller('instituciones')
 export class InstitucionesController {
-  constructor(private readonly institucionesService: InstitucionesService) {}
+  constructor(private readonly instituciones: InstitucionesService) {}
+
+  @Post()
+  @HttpCode(201)
+  @ApiOperation({ summary: 'Crear institución' })
+  create(@Body() dto: CreateInstitucionDto) {
+    return this.instituciones.create(dto);
+  }
 
   @ApiOperation({ summary: 'Listar todas las instituciones' })
   @ApiQuery({ name: 'active', example: true })
@@ -22,13 +36,46 @@ export class InstitucionesController {
     @Query('active', new DefaultValuePipe(false), ParseBoolPipe)
     active: boolean,
   ) {
-    return this.institucionesService.findAll(active);
+    return this.instituciones.findAll(active);
   }
 
   @ApiOperation({ summary: 'Obtener institución por id' })
   @ApiParam({ name: 'id', example: '1' })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.institucionesService.findOne(id);
+    return this.instituciones.findOne(id);
+  }
+
+  @Get('/nombre/:nombre')
+  @ApiOperation({ summary: 'Obtener institución por nombre' })
+  @ApiParam({ name: 'nombre', example: 'Wi-Fi' })
+  findOneName(@Param('nombre') nombre: string) {
+    return this.instituciones.findOneByName(nombre);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar institución' })
+  @ApiParam({ name: 'id', example: '1' })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateInstitucionDto,
+  ) {
+    return this.instituciones.update(id, dto);
+  }
+
+  @Delete('/status/:id')
+  @HttpCode(204)
+  @ApiOperation({ summary: 'Cambiar estado de una institución' })
+  @ApiParam({ name: 'id', example: '1' })
+  shutdown(@Param('id', ParseIntPipe) id: number) {
+    return this.instituciones.shutdown(id);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  @ApiOperation({ summary: 'Eliminarinstitucion' })
+  @ApiParam({ name: 'id', example: '1' })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.instituciones.remove(id);
   }
 }
