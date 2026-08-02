eedec86d14dd5a79a56f31d10706d5b7795c4c26
Jonathan Alberto Barrera <20233tn110@utez.edu.mx>
2026-08-01 23:29:41 -0600

    [fix] migracion

--- apps/backend/services/catalogo-service/prisma/migrations/20260801203907_ulr_institucion/migration.sql
+++ apps/backend/services/catalogo-service/prisma/migrations/20260801203907_ulr_institucion/migration.sql
@@ -1,8 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `imagen_url` to the `Institucion` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "Institucion" ADD COLUMN     "imagen_url" VARCHAR(255) NOT NULL;
+ALTER TABLE "Institucion"
+ADD COLUMN "imagen_url" VARCHAR(255) NOT NULL DEFAULT '/institucion-default.jpg';
